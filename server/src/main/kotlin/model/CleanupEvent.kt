525e501a5edc6d059ff0c53ff37dfce77cc3a7f9
Bertram Englert <bertram.engert@gmx.at>
2023-06-19 10:46:21 +0200

    CleanupEvent creation

---
+++ server/src/main/kotlin/model/CleanupEvent.kt
@@ -0,0 +1,59 @@
+package model
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object CleanupEvent : IntIdTable() {
+    val cleanupDayId = integer("cleanupDayId").references(CleanupDay.id)
+    val firstName = varchar("firstName", 100)
+    val lastName = varchar("lastName", 100)
+    val emailAddress = varchar("emailAddress", 100)
+    val organization = varchar("organization", 100)
+    val websiteAddress = varchar("website", 200)
+    val eventName = varchar("eventName", 100)
+    val street = varchar("street", 100)
+    val zipCode = varchar("zipCode", 10)
+    val image = blob("image")
+}
+
+
+class CleanupEventDao(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<CleanupEventDao>(CleanupEvent) {
+        fun insert(cleanupDayId: Int,
+                   firstName:String,
+                   lastName:String,
+                   emailAddress:String,
+                   organization:String,
+                   websiteAddress:String,
+                   eventName:String,
+                   street:String,
+                   zipCode:String,
+                   image:ByteArray) = transaction {
+            new {
+                this.cleanupDayId = cleanupDayId
+                this.firstName = firstName
+                this.lastName = lastName
+                this.emailAddress = emailAddress
+                this.organization = organization
+                this.websiteAddress = websiteAddress
+                this.eventName = eventName
+                this.street = street
+                this.zipCode = zipCode
+            }
+        }
+    }
+
+    var cleanupDayId by CleanupEvent.cleanupDayId
+    var firstName by CleanupEvent.firstName
+    var lastName by CleanupEvent.lastName
+    var emailAddress by CleanupEvent.emailAddress
+    var organization by CleanupEvent.organization
+    var websiteAddress by CleanupEvent.websiteAddress
+    var eventName by CleanupEvent.eventName
+    var street by CleanupEvent.street
+    var zipCode by CleanupEvent.zipCode
+    var image by CleanupEvent.image
+}
