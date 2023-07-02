02b9ef4c7802675248eb12a9660d11dd8cc37e56
PatrickBatkeOV <patrick.batke@openvalue.at>
2023-07-02 12:00:16 +0200

    Add ShareResults.kt page

--- browser/src/main/kotlin/pages/ShareResults.kt
+++ browser/src/main/kotlin/pages/ShareResults.kt
@@ -3,9 +3,23 @@ package pages
 import components.RoutePage
 import css.Classes
 import emotion.react.css
-import react.FC
-import react.Props
+import emotion.react.css
+import kotlinx.datetime.Instant
+import model.CreateShareResultsEntity
+import react.*
 import react.dom.html.ReactHTML
+import utils.Requests
+import web.cssom.Auto
+import web.cssom.TextAlign
+import web.html.HTMLInputElement
+import web.html.InputType
+
+data class FormField(
+    var label: String,
+    var value: String,
+    var setter: StateSetter<String>
+)
+
 
 object ShareResultsPage : RoutePage {
     override val route: String = "share-results"
@@ -13,7 +27,117 @@ object ShareResultsPage : RoutePage {
         get() = FC {
             ReactHTML.div {
                 css(Classes.limitedWidth)
-                +"Share Results Table"
+                val (mail, setMail) = useState<String>("")
+                val (entity, setEntity) = useState<String>("")
+                val (province, setProvince) = useState<String>("")
+                val (postCode, setPostCode) = useState<String>("")
+                val (location, setLocation) = useState<String>("")
+                val (dateOfCleanup, setDateOfCleanup) = useState<String>("")
+                val (amountOfParticipants, setAmountOfParticipants) = useState<Int>(0)
+                val (totalWeight, setTotalWeight) = useState<Int>(0)
+                val (amountOfTrashBags, setAmountOfTrashBags) = useState<Int>(0)
+                val (partnerOrganisation, setPartnerOrganisation) = useState<String>("")
+                val (cleanedAreaSize, setCleanedAreaSize) = useState<String>("")
+                val (cigaretteButtsCount, setCigaretteButtsCount) = useState<Int>(0)
+                val (canCount, setCanCount) = useState<Int>(0)
+                val (petBottleCount, setPetBottleCount) = useState<Int>(0)
+                val (glassBottleCount, setGlassBottleCount) = useState<Int>(0)
+                val (hazardousWaste, setHazardousWaste) = useState<String>("")
+                val (strangeFinds, setStrangeFinds) = useState<String>("")
+                val (miscellaneous, setMiscellaneous) = useState<String>("")
+                val (wayOfRecognition, setWayOfRecognition) = useState<String>("")
+
+                val formFields = listOf(
+                    FormField("Mail", mail, setMail),
+                    FormField("Entity", entity, setEntity),
+                    FormField("Province", province, setProvince),
+                    FormField("Post Code", postCode, setPostCode),
+                    FormField("Location", location, setLocation),
+                    FormField("Date Of Cleanup", dateOfCleanup, setDateOfCleanup),
+                    FormField("Amount Of Participants", amountOfParticipants, setAmountOfParticipants),
+                    FormField("Total Weight", totalWeight, setTotalWeight),
+                    FormField("Amount Of Trash Bags", amountOfTrashBags, setAmountOfTrashBags),
+                    FormField("Partner Organisation", partnerOrganisation, setPartnerOrganisation),
+                    FormField("Cleaned Area Size", cleanedAreaSize, setCleanedAreaSize),
+                    FormField("Cigarette Butts Count", cigaretteButtsCount, setCigaretteButtsCount),
+                    FormField("Can Count", canCount, setCanCount),
+                    FormField("PET Bottle Count", petBottleCount, setPetBottleCount),
+                    FormField("Glass Bottle Count", glassBottleCount, setGlassBottleCount),
+                    FormField("Hazardous Waste", hazardousWaste, setHazardousWaste),
+                    FormField("Strange Finds", strangeFinds, setStrangeFinds),
+                    FormField("Miscellaneous", miscellaneous, setMiscellaneous),
+                    FormField("Way Of Recognition", wayOfRecognition, setWayOfRecognition)
+                )
+
+
+                ReactHTML.img {
+
+                }
+
+                ReactHTML.p {
+                    +"Gemeinsam haben wir den World Cleanup Day in Österreich möglich gemacht. Danke!!"
+                }
+
+                ReactHTML.form {
+                    formFields.forEach { field ->
+                        ReactHTML.div {
+                            +"${field.label}: "
+                            ReactHTML.input {
+                                type = when (field.label) {
+                                    "Mail" -> InputType.email
+                                    "Date Of Cleanup" -> InputType.date
+                                    "totalWeight" -> InputType.number
+                                    // Add other specific input types as needed
+                                    else -> InputType.text
+                                }
+                                placeholder = ""
+                                value = field.value
+                                onChange = {
+                                    val target = it.target as HTMLInputElement
+                                    field.setter(target.value)
+                                }
+                                css {
+                                    margin = Auto.auto
+                                    textAlign = TextAlign.center
+                                }
+                            }
+                        }
+                    }
+
+                    ReactHTML.button {
+                        +"Senden"
+                    }
+
+                    onSubmit = {
+                        Requests.post("/shareresults", CreateShareResultsEntity(
+                            timestamp = Instant.parse("01012020"),
+                            mail = mail,
+                            entity = entity,
+                            province = province,
+                            postCode = postCode,
+                            location = location,
+                            dateOfCleanup = Instant.parse(dateOfCleanup),
+                            amountOfParticipants =  amountOfParticipants,
+                            totalWeight = totalWeight,
+                            amountOfTrashBags = amountOfTrashBags,
+                            partnerOrganisation = partnerOrganisation,
+                            cleanedAreaSize = cleanedAreaSize,
+                            cigaretteButtsCount = cigaretteButtsCount,
+                            canCount = canCount,
+                            petBottleCount = petBottleCount,
+                            glassBottleCount = glassBottleCount,
+                            hazardousWaste = hazardousWaste,
+                            strangeFinds = strangeFinds,
+                            miscellaneous = miscellaneous,
+                            wayOfRecognition = wayOfRecognition
+                        ))
+                    }
+
+                }
+
+                useEffect(mail) {
+                    console.log("Email")
+                }
             }
         }
 }
